e7ea273a7f9bdd01cc44802966ab053399828d12
Asad Ali <asad710931@gmail.com>
2024-09-23 22:48:35 +0600

    2nd update

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 import express from 'express'
-
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 
 const app = express()
-const port=process.env.PORT||3030;
+const __dirname=path.dirname(fileURLToPath(import.meta.url))
+app.use(express.static(path.join(__dirname,'public')))
+console.log(__dirname)
 
-
-
-
-
-app.get('/',(req,res)=>{
-    res.send(`not found`)
-})
-app.post('/post',(req,res)=>{
+//app.get('/',(req,res)=>{
+   // res.send('working Server :)')
+//})
+app.get('/post',(req,res)=>{
     res.send({'hello':'new'})
 })
-app.get('/',(req,res)=>{
-    res.send(`not found`)
-})
 
+
+
+
+const port=process.env.PORT||3030;
 
 app.listen(port,()=>{
     console.log('Server started at: '+port)
